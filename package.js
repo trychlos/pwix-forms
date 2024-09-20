a480cb5f827c90541175c192ad7a0619c21c1aef
Pierre Wieser <p.wieser@trychlos.org>
2024-09-20 16:57:01 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:forms',
-    version: '1.2.0',
+    version: '1.2.1-rc',
     summary: 'Meteor forms management',
     git: 'https://github.com/trychlos/pwix-forms.git',
     documentation: 'README.md'
